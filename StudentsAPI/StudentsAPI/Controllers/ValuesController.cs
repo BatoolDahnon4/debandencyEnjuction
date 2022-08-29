2d7b39db6326c4f34c45b4ec6a92201a4138befb
Batool_Dahnon <90872175+BatoolDahnon4@users.noreply.github.com>
2022-08-30 02:09:47 +0300

    Add files via upload

---
+++ StudentsAPI/StudentsAPI/Controllers/ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsAPI.interfaces;
+
+
+namespace StudentsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private IRepository _studentRepo;
+        public List<Student> students = new List<Student>()
+            {
+                 new Student{Id=1,  Name="maha",Age=11,City="Ramallah"},
+
+                new Student{Id=2,  Name="Jana",Age=20,City="Ramallah"},
+
+            };
+        public ValuesController(IRepository studentRepo)
+        {
+            _studentRepo = studentRepo;
+        }
+
+        [HttpGet]
+        [Route("getStudents")]
+        public async Task<ActionResult<Student>> GetSudents()
+        {
+
+            return Ok(students);
+        }
+
+        [HttpGet]
+        [Route("getStudent")]
+        public async Task<ActionResult<Student>> GetSudent(int id)
+        {
+            var student = students.Find(x => x.Id == id);
+            if (student == null)
+                return
+                    BadRequest("No students found");
+            return Ok(student);
+        }
+
+        [HttpPost]
+        [Route("addStudent")]
+        public async Task<ActionResult<Student>> AddSudent(Student request)
+        {
+            students.Add(request);
+            return Ok(students);
+        }
+
+        [HttpPut]
+        [Route("updateStudent")]
+        public async Task<ActionResult<Student>> updateSudent(Student request)
+        {
+            var student = students.Find(x => x.Id == request.Id);
+            if (student == null)
+                return BadRequest("No students found");
+            student.Name = request.Name;
+            student.Age = request.Age;
+            student.City = request.City;
+            return Ok(students);
+        }
+
+        [HttpDelete]
+        [Route("deleteStudent")]
+        public async Task<ActionResult<Student>> DeleteSudent(int id)
+        {
+            var student = students.Find(x => x.Id == id);
+            if (student == null)
+                return
+                    BadRequest("No students found");
+
+            students.Remove(student);
+            return Ok(student);
+
+        }
+    }
+}
---
+++ StudentsAPI/StudentsAPI/interfaces/IRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+
+namespace StudentsAPI.interfaces
+{
+    public interface IRepository
+    {
+        List<Student> Get();
+        Student Get(int id);
+        public void Addstudent(Student student);
+        Student updatestudent(Student request);
+        public void Delete(int id);
+    }
+
+    public class studentRepo : IRepository
+    {
+
+        public List<Student> students = new List<Student>()
+            {
+                 new Student{Id=1,  Name="maha",Age=11,City="Ramallah"},
+
+                new Student{Id=2,  Name="Jana",Age=20,City="Ramallah"},
+
+            };
+        private object student;
+
+        public void Addstudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void Delete(int id)
+        {
+            students.Remove((Student)student);
+
+        }
+
+        public List<Student> Get()
+        {
+            return students;
+        }
+
+        public Student Get(int Id)
+        {
+            return students.FirstOrDefault(f => f.Id == Id);
+        }
+
+        public Student updatestudent(Student request)
+        {
+            var student = students.Find(p => p.Id == request.Id);
+
+            student.Id = request.Id;
+            student.Name = request.Name;
+            student.Age = request.Age;
+            student.City = request.City;
+            return student;
+
+        }
+
+        Student IRepository.updatestudent(Student request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
